18d5fa5100dfe4ff987e4c6ba55013827d5f56b8
Gautam <gotmprajapat00@gmail.com>
2026-06-28 17:25:10 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// BattelG App
+
+console.log("BattelG Started");
+
+// Loading Animation
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
+
+// Future Firebase Initialization Here
