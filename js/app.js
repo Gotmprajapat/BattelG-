16a19b1db0db2c2cbaf79c8796399a954ba7065a
Gautam <gotmprajapat00@gmail.com>
2026-07-01 22:51:54 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-import { app, auth, db } from "../firebase/firebase.js";
+import { auth } from "../firebase/firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-console.log("BattelG Started");
-console.log("Firebase Connected");
+onAuthStateChanged(auth, (user) => {
 
-window.addEventListener("load", () => {
-    document.body.style.opacity = "1";
+    // Agar user login hai aur login/register page par hai
+    if (user) {
+        if (
+            window.location.pathname.includes("index.html") ||
+            window.location.pathname.includes("login.html") ||
+            window.location.pathname.includes("register.html") ||
+            window.location.pathname === "/"
+        ) {
+            window.location.href = "home.html";
+        }
+    } else {
+        // Agar login nahi hai aur protected page khola
+        if (
+            window.location.pathname.includes("home.html") ||
+            window.location.pathname.includes("wallet.html") ||
+            window.location.pathname.includes("deposit.html")
+        ) {
+            window.location.href = "login.html";
+        }
+    }
+
 });
