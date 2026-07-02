2052261a5a275da1a74fcb30330b33908331a649
Gautam <gotmprajapat00@gmail.com>
2026-07-02 23:05:17 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -9,41 +9,42 @@ import {
   getDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+// Elements
 const userName = document.getElementById("userName");
 const walletBalance = document.getElementById("walletBalance");
 
+// Login Check
 onAuthStateChanged(auth, async (user) => {
 
-  if (!user) {
-    window.location.href = "login.html";
-    return;
-  }
-
-  try {
-
-    const userRef = doc(db, "users", user.uid);
-    const userSnap = await getDoc(userRef);
-
-    if (userSnap.exists()) {
-
-      const data = userSnap.data();
-
-      userName.textContent = data.name || "Player";
-      walletBalance.textContent = data.wallet || 0;
-
-    } else {
-
-      userName.textContent = "Player";
-      walletBalance.textContent = "0";
-
+    if (!user) {
+        window.location.href = "login.html";
+        return;
     }
 
-  } catch (error) {
+    try {
 
-    console.error("Home Error:", error);
+        const userRef = doc(db, "users", user.uid);
+        const userSnap = await getDoc(userRef);
 
-    alert("Unable to load user data.");
+        if (userSnap.exists()) {
 
-  }
+            const data = userSnap.data();
+
+            userName.innerText = data.name || "Player";
+            walletBalance.innerText = data.wallet || 0;
+
+        } else {
+
+            userName.innerText = "Player";
+            walletBalance.innerText = 0;
+
+        }
+
+    } catch (error) {
+
+        console.error("Home Error:", error);
+        alert("Failed to load user data.");
+
+    }
 
 });
