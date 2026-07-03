9fa4a92480e4a2dcb36ccd89e97d2b92600af1c2
Gautam <gotmprajapat00@gmail.com>
2026-07-03 17:11:24 +0530

    Delete js/splash.js

--- js/splash.js
+++
@@ -1,9 +0,0 @@
-firebase.auth().onAuthStateChanged((user) => {
-    setTimeout(() => {
-        if (user) {
-            window.location.href = "home.html";
-        } else {
-            window.location.href = "login.html";
-        }
-    }, 2500);
-});
